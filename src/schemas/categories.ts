c1a37fc129a7bf2a039b45392d2a1a47b80f2a38
Geraldo Luiz <geraldobl58@gmail.com>
2024-10-21 14:37:19 -0300

    feat: Create schema categories form

---
+++ src/schemas/categories.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export type FormSchema = z.infer<typeof formSchema>;
+
+export const formSchema = z.object({
+  title: z
+    .string()
+    .min(2, {
+      message: "A categoria precisa conter pelo menos 2 caracteres!",
+    })
+    .regex(/^[^\d]*$/, {
+      message: "A categoria não pode conter números!",
+    }),
+});
