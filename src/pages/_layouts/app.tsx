09d45f211bbc9c09528323882ca8df62e1fda354
Geraldo Luiz <geraldobl58@gmail.com>
2024-10-21 11:16:14 -0300

    feat: Creating header and sidebar component with links

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,11 +1,72 @@
 import { Outlet } from "react-router-dom";
 
+import {
+  BadgeDollarSign,
+  BarChart,
+  CircleUser,
+  LayoutDashboard,
+  LocateFixed,
+  PackageSearch,
+} from "lucide-react";
+
+import { Header } from "@/components/header";
+import { NavLink } from "@/components/nav-links";
+
+const routes = [
+  {
+    to: "/",
+    icon: LayoutDashboard,
+    label: "Dashboard",
+  },
+  {
+    to: "/analytics",
+    icon: BarChart,
+    label: "Analytics",
+  },
+  {
+    to: "/categories",
+    icon: LocateFixed,
+    label: "Categorias",
+  },
+  {
+    to: "/products",
+    icon: PackageSearch,
+    label: "Produtos",
+  },
+  {
+    to: "/orders",
+    icon: BadgeDollarSign,
+    label: "Pedidos",
+  },
+  {
+    to: "/clients",
+    icon: CircleUser,
+    label: "Clientes",
+  },
+];
+
 export function AppLayout() {
   return (
-    <div>
-      <div>
-        <div>Header</div>
-        <Outlet />
+    <div className="antialiased">
+      <Header />
+      <div className="flex min-h-screen">
+        <aside className="bg-white w-[240px]">
+          <nav>
+            <ul>
+              {routes.map((item) => (
+                <NavLink
+                  key={item.to}
+                  to={item.to}
+                  icon={<item.icon className="size-4" />}
+                  label={item.label}
+                />
+              ))}
+            </ul>
+          </nav>
+        </aside>
+        <main className="flex-1 p-8 bg-gray-100">
+          <Outlet />
+        </main>
       </div>
     </div>
   );
--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,6 +1,6 @@
 export function Dashboard() {
   return (
-    <div>
+    <div className="bg-slate-400 flex-1 min-h-screen">
       <div>
         <div>Dashboard</div>
       </div>
